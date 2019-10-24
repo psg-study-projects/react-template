375b5e094f31983c4be01e4acdebeb7c6527244d
Peter Gorgone <peter@peltronic.com>
2019-10-24 11:11:54 -0700

    working hello world with simple react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import Alert from './components/layout/Alert';
+//import Navbar from './components/layout/Navbar';
+//import Landing from './components/layout/Landing';
+//import Login from './components/auth/Login';
+//import Dashboard from './components/dashboard/Dashboard';
+import Stub from './components/dashboard/Stub';
+//import CreateProfile from './components/profile-forms/CreateProfile';
+//import EditProfile from './components/profile-forms/EditProfile';
+//import AddExperience from './components/profile-forms/AddExperience';
+//import AddEducation from './components/profile-forms/AddEducation';
+//import Profiles from './components/profiles/Profiles';
+//import Profile from './components/profile/Profile';
+//import Posts from './components/posts/Posts';
+//import Post from './components/post/Post';
+//import PrivateRoute from './components/routing/PrivateRoute';
+//import Register from './components/auth/Register';
+
+// Redux
+import { Provider } from 'react-redux';
+//import store from './store';
+//import { loadUser } from './actions/auth';
+//import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello test src
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
 }
+*/
+
+const App = () => {
+
+    /*
+        // use Effect hook %TODO %STUDYME
+    useEffect(() => {
+        // ~~ componentDidMount()
+        store.dispatch(loadUser());
+    }, []); // 2nd paramter with empty brackets prevents this from running continuously (ie doesnt'depend on anything from props or state)
+    */
+
+        return (
+                <Router>
+                    <Fragment>
+                        {/*
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    */}
+                    <section className="container">
+                        {/*
+                        <Alert />
+                        */}
+                        <Switch>
+                            <Route path='/dashboard' component={Stub} />
+                            <Route path='/foo' component={Stub} />
+                            {/*
+                            <Route exact path='/register' component={Register} />
+                            <Route exact path='/login' component={Login} />
+                            <Route exact path='/profiles' component={Profiles} />
+                            <Route exact path='/profile/:id' component={Profile} />
+                            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+                            <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+                            <PrivateRoute exact path='/add-experience' component={AddExperience} />
+                            <PrivateRoute exact path='/add-education' component={AddEducation} />
+                            <PrivateRoute exact path='/posts' component={Posts} />
+                            <PrivateRoute exact path='/posts/:id' component={Post} />
+                            */}
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        )
+};
 
 export default App;
+
+
+{/*
+<Provider store={store}>
+</Provider>
+*/}
---
+++ src/components/dashboard/Stub.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useEffect } from 'react';
+import { connect }  from 'react-redux';
+
+const Stub = () => {
+
+    return <h3>Stub Dashboard</h3>;
+
+};
+
+//export default connect()(Stub);
+export default Stub;
